88f2e517713c8cc511786e4cd001a5554f9d1cae
thedistortedwajdan <wajdan.mohammad@gmail.com>
2024-01-12 23:36:24 +0500

    components setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <Box>
+        <Navbar />
+        <Feed />
+        <Rightbar />
+        <Sidebar />
+      </Box>
     </>
   );
 }
---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Rightbar() {
+  return <div>Rightbar</div>;
+}
