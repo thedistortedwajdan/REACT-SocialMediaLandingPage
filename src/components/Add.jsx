8ceeb11408e32d6e48667980946881e2b0532bc6
thedistortedwajdan <wajdan.mohammad@gmail.com>
2024-02-04 19:59:56 +0500

    feb 4 2024, Create Post modal

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,11 +1,40 @@
-import { Fab, Tooltip } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
+import {
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from "@mui/icons-material";
 
 export default function Add() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #1976D2",
+    borderRadius: "20px",
+    boxShadow: 24,
+    p: 4,
+  };
   return (
     <>
       <Tooltip
+        onClick={handleOpen}
         title="Add a Post?"
         sx={{
           position: "fixed",
@@ -20,6 +49,46 @@ export default function Add() {
           <AddIcon />
         </Fab>
       </Tooltip>
+      <Modal
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            textAlign={"center"}
+            variant="h6"
+            fontWeight={700}
+            color="#1976D2"
+          >
+            Create Post
+          </Typography>
+          <TextField
+            sx={{
+              width: "100%",
+            }}
+            multiline
+            rows={3}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+          <Stack direction={"row"} gap={1} mt={2} mb={3}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
+          <ButtonGroup fullWidth variant="contained">
+            <Button>Post</Button>
+          </ButtonGroup>
+        </Box>
+      </Modal>
     </>
   );
 }
